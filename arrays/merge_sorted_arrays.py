03b8156ba14a6edfc6478f2f7b3acdb860c47520
Maximiliano Delbono <maximilianodelbono@gmail.com>
2021-04-12 09:37:22 -0700

    sort and merge v1 running

--- arrays/merge_sorted_arrays.py
+++ arrays/merge_sorted_arrays.py
@@ -1,18 +1,24 @@
-arr_1=[0,3,4,31]
-arr_2=[4,6,30]
+arr_1=[0,3,4,31,32]
+arr_2=[4,6,30,60]
 arr_res=[]
 
-for i in arr_1:
-    for j in arr_2:
-        if(i<j):
-            arr_res.append(i)
-            break
-        elif (i==j):
-            arr_res.append(i)
-            arr_res.append(j)
-            break
-        elif (i>j):
-            arr_res.append(j)
-            arr_res.append(i)
-            break
-print(arr_res)
+def mergeSortedarr(a,b):
+    '''Funciton to sort and merge'''
+    if len(a)==0 or len(b)==0:
+        #print("one of the arrays is empty")
+        return a+b
+    i=0
+    j=0
+
+    while i<len(a) and j<len(b):
+        if a[i]<=b[j]:
+            arr_res.append(a[i])
+            i+=1
+        elif a[i]>b[j]:
+            arr_res.append(b[j])
+            j+=1
+    print("•i",i,a[i:])
+    print(b[j:])
+    return arr_res+a[i:]+b[j:]
+
+print(mergeSortedarr(arr_1,arr_2))
